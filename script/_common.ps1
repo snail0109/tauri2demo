1136f9d52db1e9a8da1e968952473b02b6f6adfc
唐林小宝 <lqq1119@sina.com>
2026-04-27 22:45:59 +0800

    fix: (2026-04-27 22:45:59)

--- script/_common.ps1
+++ script/_common.ps1
@@ -511,7 +511,7 @@ function Save-WebFile {
       $shortName = $uri.Host
     } catch { $shortName = $ctx.Url }
     if ($ctx.Error) {
-      Write-Host ("    ✗ {0}  {1}" -f $shortName, $ctx.Error) -ForegroundColor Red
+      Write-Host ("    ✗ {0}" -f $shortName) -ForegroundColor Red
     } else {
       $spdKB = [int](($ctx.Bytes / $elapsed) / 1KB)
       Write-Host ("    {0}  {1:N0} KB  {2:N0} KB/s" -f $shortName, ($ctx.Bytes / 1KB), $spdKB) -ForegroundColor Cyan
